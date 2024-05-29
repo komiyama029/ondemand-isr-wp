d8df74ee4a70817e86f94d79d72675947272a12b
komiyama029 <komiyama@ekanlab.co.jp>
2024-05-29 14:59:09 +0900

    APIが2回呼ばれないように修正

--- functions.php
+++ functions.php
@@ -1,23 +1,33 @@
 <?php
 
-function revalidate_post($post_id) {
-	$post = get_post($post_id);
+function revalidate_post($post_id, $post_after, $post_before) {
+	if ($post_after->post_modified_gmt !== $post_before->post_modified_gmt) {
+		$post = get_post($post_id);
 
-	$api_url = 'http://localhost:3000/api/revalidate';
-
-	$response = wp_remote_post($api_url, array(
-		'body' => array(
-			'path' => '/post/' . $post_id,
-			'token' => REVALIDATION_TOKEN,
-		),
-	));
-
-	// 応答のチェックとエラーログ
-	if (is_wp_error($response)) {
-		error_log('Revalidate API request failed: ' . $response->get_error_message());
-	} else if (wp_remote_retrieve_response_code($response) !== 200) {
-		error_log('Revalidate API request error. Status Code: ' . wp_remote_retrieve_response_code($response));
+		$api_url = 'http://localhost:3000/api/revalidate';
+	
+		$response = wp_remote_post($api_url, array(
+			'body' => array(
+				'path' => '/post/' . $post_id,
+				'token' => REVALIDATION_TOKEN,
+			),
+		));
+	
+		// 応答のチェックとエラーログ
+		if (is_wp_error($response)) {
+			error_log('Revalidate API request failed: ' . $response->get_error_message());
+		} else {
+			$status_code = wp_remote_retrieve_response_code($response);
+			$response_body = wp_remote_retrieve_body($response);
+	
+			if ($status_code !== 200) {
+				error_log('Revalidate API request error. Status Code: ' . $status_code . '. Response: ' . $response_body);
+			} else {
+				error_log('Revalidate API request succeeded. Response: ' . $response_body);
+			}
+		}
 	}
+	
 }
 
-add_action("save_post", "revalidate_post", 20, 1);
+add_action("post_updated", "revalidate_post", 20, 3);
